38b8e8cb46ad013ff8ac05082270efd028ccebcf
ThanaphatPisan <s6704062616222@email.kmutnb.ac.th>
2024-11-28 17:59:20 +0700

    Another Version Better than Version1

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main() {
-	int num1=10;
-	int num2=5;
+	int num1=52;
+	int num2=67;
 	if (num1>num2){
 		printf("num1 is greater than num2");
 	}
-	if (num2>num1){
+	else{
 		printf("num2 is greater than num1");
 	}
+	return 0;
 }
 
