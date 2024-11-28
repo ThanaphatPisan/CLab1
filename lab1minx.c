7fa578b8f407944ccea92bb0a33e187b3aa963d2
ThanaphatPisan <s6704062616222@email.kmutnb.ac.th>
2024-11-28 18:19:56 +0700

    better than V2 original is the new V2

--- lab1minx.c
+++ lab1minx.c
@@ -8,9 +8,11 @@ int main() {
 	scanf("%d",&num2);
 	if (num1>num2){
 		printf("num1 is greater than num2");
+		printf("\n%d",(num1-num2));
 	}
 	else{
 		printf("num2 is greater than num1");
+		printf("\n%d",(num2-num1));
 	}
 	return 0;
 }
